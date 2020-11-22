1c2039a8ff9bc607982c35c79632cc1632f7de3e
Ivan Dimitrov <biobeehive@abv.bg>
2020-11-22 08:25:12 +0200

    Initial Commit

---
+++ ProbaTextAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProbaTextAnimation
+//
+//  Created by Ivan Dimitrov on 22.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProbaTextAnimation/ProbaTextAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbaTextAnimationApp.swift
+//  ProbaTextAnimation
+//
+//  Created by Ivan Dimitrov on 22.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ProbaTextAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
